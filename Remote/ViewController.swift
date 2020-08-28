2bdff4ab30ff19b91dba5dde1acbaafde37993d6
Jack Weber <jackdweber@gmail.com>
2020-08-27 17:51:10 -0700

    Initial Commit

---
+++ Remote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Remote
+//
+//  Created by Jack Weber on 8/27/20.
+//  Copyright © 2020 Jack Weber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
